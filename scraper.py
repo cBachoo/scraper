d0bedd9fa5d94e349a12189d8bfd6781c023de60
cBachoo <cosmin_cb@hotmail.com>
2026-07-07 10:15:21 -0400

    chromium fix test

--- scraper.py
+++ scraper.py
@@ -25,7 +25,7 @@ def setup_chrome_driver():
     """Configure and return a Chrome WebDriver instance."""
     options = webdriver.ChromeOptions()
     options.binary_location = "/usr/bin/chromium"
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-web-security")
     options.add_argument("--allow-running-insecure-content")
     options.add_argument(
@@ -34,6 +34,10 @@ def setup_chrome_driver():
     )
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
 
     return webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
 
